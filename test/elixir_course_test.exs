d3aa28f21c19abadfc3deb3a402c095f7550f818
Piotr Świderski <piotrswiderski@MacBook-Pro-Piotr.local>
2021-07-02 22:43:33 +0200

    simple tests added

--- test/elixir_course_test.exs
+++ test/elixir_course_test.exs
@@ -1,8 +1,14 @@
 defmodule ElixirCourseTest do
   use ExUnit.Case
-  doctest ElixirCourse
+  doctest Cards
 
-  test "greets the world" do
-    assert ElixirCourse.hello() == :world
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 52
+  end
+
+  test "shuffling" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
   end
 end
